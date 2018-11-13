43c0e216004fb1dd617519edf206fa64ffb75f18
Andrew Fox <andre69charger@gmail.com>
2018-11-12 22:24:04 -0500

    Add pre-commit hooks and grunt task for testing.

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        login: './src/main/webapp/resources/theme1/js/login/loginApp.js',
-        app: './src/main/webapp/resources/theme1/js/app/index.js'
-    },
-    cache: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/webapp/resources/theme1/js/built/[name]-bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+  entry: {
+    login: "./src/main/webapp/resources/theme1/js/login/loginApp.js",
+    app: "./src/main/webapp/resources/theme1/js/app/index.js"
+  },
+  cache: true,
+  output: {
+    path: __dirname,
+    filename: "./src/main/webapp/resources/theme1/js/built/[name]-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,15 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
-const webpack = require('webpack');
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const webpack = require("webpack");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'sourcemaps',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development')
-            }
-        })
-    ]
+  mode: "development",
+  devtool: "sourcemaps",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    })
+  ]
 });
